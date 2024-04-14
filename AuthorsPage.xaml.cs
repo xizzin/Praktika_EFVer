58967822a37f0529d43d373a5c0bed0dea9ec775
xizzin <xizzincol@gmail.com>
2024-04-15 00:56:52 +0300

    first ef practice done

---
+++ AuthorsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prOneEF
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorsPage.xaml
+    /// </summary>
+    public partial class AuthorsPage : Page
+    {
+        private booksEntities BooksDB = new booksEntities();
+        public AuthorsPage()
+        {
+            InitializeComponent();
+            AuthorsGrid.ItemsSource = BooksDB.Authors.ToList();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prOneEF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OneButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorsPage authorsPage = new AuthorsPage();
+            AuthorsFrame.Content = authorsPage;
+        }
+
+        private void TwoButton_Click(object sender, RoutedEventArgs e)
+        {
+            GenresPage genresPage = new GenresPage();
+            GenresFrame.Content = genresPage;
+        }
+
+        private void ThreeButton_Click(object sender, RoutedEventArgs e)
+        {
+            BooksPage booksPage = new BooksPage();
+            BooksFrame.Content = booksPage;
+        }
+
+    }
+}
